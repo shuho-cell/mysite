ab4fcb5a2ee823c60cdf8ad82196c7e8fd76d5df
Kato Shuho <showing838@gmail.com>
2020-05-14 17:05:51 +0900

    error dom

---
+++ practice_vue.js/js/v-if_v-show.js
@@ -0,0 +1,20 @@
+new Vue ({
+  el: '#app1',
+  // el: '#app2',
+  data: {
+    // message : 'OK',
+    // message2 : 'Not Ok!',
+    item: 'true',
+    // templateIf: 'true',
+  }
+})
+// new Vue ({
+//   el: '#app2',
+//   data: {
+//     templateIf: 'true',
+
+//   }
+// })
+
+
+// new vueを複数作ると、後ろのほうが適応される
