e3f7ccc787254a551bd0d2c5f09a95169a65491b
Kato Shuho <showing838@gmail.com>
2020-05-17 22:57:44 +0900

    外側からのVueに追加

--- practice_vue.js/js/v-if_v-show.js
+++ practice_vue.js/js/v-if_v-show.js
@@ -1,20 +1,24 @@
-new Vue ({
+var vm1 = new Vue ({
   el: '#app1',
-  // el: '#app2',
   data: {
-    // message : 'OK',
-    // message2 : 'Not Ok!',
+    message : 'OK',
+    message2 : 'Not Ok!',
     item: 'true',
-    // templateIf: 'true',
   }
 })
-// new Vue ({
-//   el: '#app2',
-//   data: {
-//     templateIf: 'true',
+new Vue ({
+  el: '#app2',
+  data: {
+    templateIf: 'true',
 
-//   }
-// })
+  },
+  methods: {
+    changeMessage: function() {
+      vm1.message = 'app2より変えられました',
+      vm1.message2 = 'app2よりmessage2が変えられました'
+    }
+  }
+})
 
 
 // new vueを複数作ると、後ろのほうが適応される
